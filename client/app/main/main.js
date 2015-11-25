91c32e61bd45564d1e19f8c7139f4a7086d8bd58
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-11-25 10:24:09 +0530

    initialized the project using yeoman

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('rwaApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
