bf9cf3856f4e92eda1a3296e072ea5886b1891b8
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-12-18 00:21:33 +0530

    fixed the fb login issue

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -5,9 +5,7 @@ var passport = require('passport');
 var auth = require('../auth.service');
 
 var router = express.Router();
-
 router.post('/', function (req, res, next) {
-
     passport.authenticate('local', function (err, user, info) {
         var error = err || info;
         if (error) return res.status(401).json(error);
