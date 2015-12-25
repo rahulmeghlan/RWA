f8cd1c0440f40a9b3b99fafab2d38ed52d2595e6
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-12-25 19:58:42 +0530

    added a new home route to create a namespace for the new functionalities

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,29 +1,30 @@
 'use strict';
 
 angular.module('rwaApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
-    $scope.user = {};
-    $scope.errors = {};
+    .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
+        $scope.user = {};
+        $scope.errors = {};
 
-    $scope.login = function(form) {
-      $scope.submitted = true;
+        $scope.login = function (form) {
+            $scope.submitted = true;
 
-      if(form.$valid) {
-        Auth.login({
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
-      }
-    };
+            if (form.$valid) {
+                Auth.login({
+                    email: $scope.user.email,
+                    password: $scope.user.password
+                })
+                    .then(function () {
+                        // Logged in, redirect to home
+//                        var path = Auth.isAdmin() ? "/admin" : "/home";
+                        $location.path("/home");
+                    })
+                    .catch(function (err) {
+                        $scope.errors.other = err.message;
+                    });
+            }
+        };
 
-    $scope.loginOauth = function(provider) {
-      $window.location.href = '/auth/' + provider;
-    };
-  });
+        $scope.loginOauth = function (provider) {
+            $window.location.href = '/auth/' + provider;
+        };
+    });
